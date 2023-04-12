bc53390948b7793e1a433066cb8d3d07cd4dcc6a
Frederick J. Boehm <frederick.boehm@gmail.com>
2023-04-12 13:56:25 -0400

    updated R functino

--- R/assemble_pgs.R
+++ R/assemble_pgs.R
@@ -1,22 +1,30 @@
-#' Assemble pgs 
+#' Assemble pgs from a collection of chromosome-specific pgs files
 #' 
 #' @param pgs_stem a character vector of the file name stem for the pgs files, which are typically the outputs from plink score allele command.
 #' @param chr_vec an integer vector indicating the chromosome numbers to be included in the pgs
-#' 
+#' @param subject_ids_column_number an integer indicating the column number of the subject ids in the pgs files
+#' @param score_column_number an integer indicating the column number of the pgs scores in the pgs files
+#' @return a data frame with two columns, the first being the subject ids and the second being the pgs scores
+#' @export
+#' @details 
+#' The function reads the PGS files, which are typically outputted by plink score allele. 
+#' Each chromosome is assumed to have its own file, and the function reads in each file 
+#' and adds the scores together. The function assumes that the subject ids are in the same
+#' column in each file, and that the pgs scores are in the same column in each file.
+#' The function also assumes that the chromosome-specific pgs files are named in the specified way.
 
-assemble_pgs <- function(pgs_stem, chr_vec){
-
+assemble_pgs <- function(pgs_stem, chr_vec, subject_ids_column_number, score_column_number){
     for (chr_num in chr_vec){
         pgs_fn <- paste0(pgs_stem, "_chr", chr_num, "_best.dbslmm.txt.profile")
         pgs <- readr::read_table(pgs_fn)
         if (chr_num == 1){
-            ss[[chr_num]] <- out2[[chr_num]] %>%
-                dplyr::select(1, 6)  # choose only the SCORESUM column, unless it's chr1
-            colnames(ss[[chr_num]])[2] <- paste0(colnames(ss[[chr_num]])[2], "_chr", chr_num) 
+            ss <- pgs %>%
+                dplyr::select(subject_ids_column_number, score_column_number)  # choose only the SCORESUM column, unless it's chr1
         } else {
-            ss[[chr_num]] <- out2[[cross_num]][[chr_num]] %>%
-                dplyr::select(6)  # choose only the SCORESUM column
-            colnames(ss[[chr_num]]) <- paste0(colnames(ss[[chr_num]]), "_chr", chr_num) 
+            ss <- ss %>%
+                dplyr::mutate(SCORESUM = SCORESUM + pgs$SCORESUM)
         }
     }
+    return(ss)
 }
+
